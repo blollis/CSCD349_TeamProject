126286eac0b5ae40f2d8836347018fb67613d844
Sierra Huckins <chewygranola89@eagles.ewu.edu>
2015-05-01 11:16:45 -0700

    Party Class
    Party Class

---
+++ CharacterClasses/Party.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Party {
+   ArrayList<Character> partyMembers;
+   
+	public Party() {
+   
+	}//close Elf
+   
+   public void addMember(Character newMember) {
+      partyMembers.add(newMember);  
+   }
+
+}//close Elf
