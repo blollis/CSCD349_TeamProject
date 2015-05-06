c7169d26179f6a83be7d6f3566a7667d42bf0556
Sierra Huckins <chewygranola89@eagles.ewu.edu>
2015-05-06 11:19:54 -0700

    New Checkpoint Wk 5

--- Checkpoint - Wk 5/Battle.java
+++ Checkpoint - Wk 5/Battle.java
@@ -1,19 +1,3 @@
-//////////////////////////////////
-//Player walks around the map until an enemy appears.
-//which then triggers a new Encounter;
-//
-//boolean victory;
-//Encounter battle = new Encounter(heroes);
-//victory = battle.combat();
-//
-//where heroes is an ArrayList<GoodGuy> of the players characters
-//when the battle is created it randomly selects a monster and how many
-//combat(); determines the turnorder of combat as well as goes through each turn
-//until either the GoodGuy List or the BadGuy List is empty.
-//if GoodGuy list is empty all the heroes died and it returns false
-//if badguy list is empty all the enemies died and it returns true
-//////////////////////////////////
-
 import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -90,6 +74,10 @@ public class Battle
                   System.out.println("You defeated " + badGuyUnderAttack.getName() + "!");
                   
                   badGuys.removeMember(badGuyUnderAttackPos);
+                  
+                  if (currentBadPos == badGuyUnderAttackPos) {
+                     currentBadPos++;
+                  }
                }  
             
                //advance turn order
@@ -121,12 +109,25 @@ public class Battle
                System.out.println(currentBadGuy.getName() + " defeated " + goodGuyUnderAttack.getName() + "!");
                
                goodGuys.removeMember(goodGuyUnderAttackPos);
+               
+               if (currentGoodPos == goodGuyUnderAttackPos) {
+                     currentGoodPos++;
+                  }
             }   
             
             //advance turn order
             currentBadPos++;
  
          }//close else
+         
+         //move turn order pointers back if they're at the end of the party
+         if (currentGoodPos >= goodGuys.size()) {
+            currentGoodPos = 0;
+         } 
+         
+         if (currentBadPos >= badGuys.size()) {
+            currentBadPos = 0;
+         }     
             
       }//while party alive
       
@@ -246,157 +247,4 @@ public class Battle
       
    }//close printStats
    
-  /*
-
-
-   //////////////////////////////
-   //Passes a good and bad guy
-   //Prints the menu for the player to chose
-   //////////////////////////////
-   public boolean attackMenu(Character player, Character enemy)
-   {
-      boolean check = false;
-      Scanner kb = new Scanner(System.in);
-      System.out.println("\nWhat to do:" +
-                         "\n1) Normal attack" +        
-                         "\n2) Special attack" +
-                         "\n3) Potions" +
-                         "\n3) Party members");
-      int choice = (kb.nextInt());
-      if(choice == 1)
-			check = combat(player, enemy);
-		else if(choice == 2)
-			check = combat(player, enemy);
-		else if(choice == 3)
-			potionMenu(); //Haven't wrote yet
-      else
-         partyMenu(player, enemy); //Haven't wrote yet
-      return check;
-   }
-   
-   public void potionMenu()
-   {
-   }
-   
-   public void partyMenu(Character p, character e)
-   {
-   }
-   
-   //////////////////////////////
-   //Passes in hero attacker and the arrayList of bad guys
-   //Prints each enemy and their remaining life from the array
-   //Has the user pick an enemy to attack or flee.
-   //////////////////////////////
-   public ArrayList evilMenu(Character player, ArrayList<BadGuy> v)
-   {
-      boolean check = false;
-      Scanner kb = new Scanner(System.in);
-      System.out.println("\nChoose enemy to attack");
-      int i;
-      for(i = 0; i < v.size(); i++)
-      {
-         System.out.println("\n" + i+1 + ") " + v.get(i).getName() + " HP: " + v.get(i).getHP());
-      }
-      System.out.println("\n0) Flee"); // Option to run
-      int choice = (kb.nextInt());
-      if(choice == 0)
-			v = flee(v);
-      else
-			check = attackMenu(player, v.get(choice-1));
-      
-      if(check)
-         v.remove(choice-1);
-         
-      return v;
-   }
-   
-   //////////////////////////////
-   //Passes in an attacker and defender character
-   //Checks to see if the attacker misses the defender
-   //If hit, the dmg is created and subtracted from the 
-   //    the defender's def stat
-   //If misses, it is printed
-   //Checks the defender's life after the take
-   //If the life is below 0, the defender is removed.
-   //////////////////////////////
-   public boolean battle(Character attacker, Character defender)
-   {
-      System.out.println(attacker.getName() + " attacks " + defender.getName());
-      int evasion = defender.getEva();
-      Random rand = new Random();
-      if((rand.nextInt(99) + 1) > evasion) 
-      {
-         int dmg = attaker.getAtk();
-         Random randomGenerator = new Random();
-         dmg = randomGenerator.nextInt(dmg) + 1;
-         dmg = dmg - defender.getDef();
-         System.out.println(attacker.getName() + " hit for " + dmg + " damage.");
-         if(dmg > 0)
-         {
-            int newHP = defender.getHp() - dmg;
-            defender.setHp(newHP);
-            if(defender.getHp() <= 0)
-               return true;
-            else
-               return false;
-         }
-      }
-      else
-         System.out.println(attacker.getName() + " missed.");
-   }//End of Combat()
-   
-   
-   //////////////////////////////
-   //Set the enemies life to 0
-   //Call setFlee() method
-   //////////////////////////////   
-   public void flee(ArrayList<BadGuy> v)
-   {
-      System.out.println("You ran away");
-      for(int i = 0; i < v.size(); i++)
-      {
-         v.get(i).setHp(0);
-      }
-      setFlee();
-   }
-   
-   
-   //////////////////////////////
-   //Set boolean flee to true
-   ////////////////////////////// 
-   public void setFlee()
-   {
-      flee = true;
-   }
-   
-   
-   //////////////////////////////
-   //Randomly generate a number to return to select a hero.
-   ////////////////////////////// 
-   public int enemySelectHero(int i)
-   {
-      Random randomGenerator = new Random();
-		int choice = randomGenerator.nextInt(i);
-		return choice;
-   }
-   
-   public static void selectionSort(int[] a) {
-      int i, j, ix, temp;
-      
-      for (i = a.length - 1; i > 0; i--) {
-         ix = 0;
-         
-         for (j = 1; j <= i; j++) {  
-            if (a[j] > a[ix]) {
-               ix = j;
-            }//close if
-         }//close for 
-      
-         temp = a[ix];
-         a[ix] = a[i];
-         a[i] = temp;
-      
-      }//close for
-      
-   }//close selectionSort*/
 } //End of Battle Class
